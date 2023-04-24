38d8f30848d87a19b7381db8a6b314f6e14f4539
Stinguerx <Stinguerx@users.noreply.github.com>
2023-04-23 23:32:46 -0400

    Refactored card class for better encapsulation, lacks equals methods.

--- src/main/scala/gwent/Card.scala
+++ src/main/scala/gwent/Card.scala
@@ -1,55 +1,62 @@
 package cl.uchile.dcc
 package gwent
 
-/** Trait that defines the attributes of a card in Gwent and behaviours of a card in Gwent*/
-trait Card {
-  /** @return the name of the card. */
-  def getName: String
-
-  /** @return the classification of the card. */
-  def getClassification: String
-
-  /** @return the effect (if any) of the card. */
-  def getEffect: Option[String]
-  
-  /** @return the default strength of the card. */
-  def getOriginalStrength: Int
-
-  /** @return the current of the card. */
-  def getCurrentStrength: Int
-
-  /** Sets the current strength of the card to the value passed.
-   *
-   * @param str the new current strength value of the card
-   */
-  def setStrength(str: Int): Unit
+/** Abstract class that defines the common attributes of all cards in Gwent. */
+abstract class Card(protected val _name: String) {
+  /** @return The name of the card.*/
+  def name: String = _name
 }
 
-/** Class that implements the uCard trait. The currentStrength attribute gets initialized to the value of
- *  the strength parameter, then it gets modified taking in consideration the effect of other Unit or Weather
- *  cards in the table.
- *
- * @param name the name of the card.
- * @param classification the classification of the card, be it melee, range, siege or weather.
- * @param strength the default strength of the card.
- * @param effect the effect (if any) that the card has over the game when played.
- * */
-class genericCard(name: String, classification: String, strength: Int, effect: Option[String]) extends Card {
-  private var currentStrength = strength
 
-  def getName: String = name
+/** Abstract class that defines the common attributes of all unit cards in Gwent. */
+abstract class UnitCard(name: String, protected val _strength: Int) extends Card(name) {
+  private var _currentStrength: Int = _strength
 
-  def getClassification: String = classification
+  /** @return The default strength of the card. */
+  def strength: Int = _strength
 
-  def getOriginalStrength: Int = strength
+  /** @return The current strength of the card. */
+  def currentStrength: Int = _currentStrength
 
-  def getCurrentStrength: Int = currentStrength
-
-  def setStrength(str: Int): Unit = {
-    currentStrength = str
+  /** Sets the current strength of the card.
+   *  @param newStrength The new strength of the card.
+   * */
+  def currentStrength_=(newStrength: Int): Unit = {
+    _currentStrength = newStrength
   }
-
-  def getEffect: Option[String] = effect
-
 }
 
+
+/** Class used for instantiating melee type cards in Gwent.
+ *
+ *  @param name The name of the card.
+ *  @param strength The strength of the card.
+ * */
+class MeleeCard(name: String, strength: Int) extends UnitCard(name, strength) {
+}
+
+
+/** Class used for instantiating ranged type cards in Gwent.
+ *
+ *  @param name The name of the card.
+ *  @param strength The strength of the card.
+ *  */
+class RangedCard(name: String, strength: Int) extends UnitCard(name, strength) {
+}
+
+
+/** Class used for instantiating siege type cards in Gwent.
+ *
+ *  @param name The name of the card.
+ *  @param strength The strength of the card.
+ *  */
+class SiegeCard(name: String, strength: Int) extends UnitCard(name, strength) {
+}
+
+
+/** Class used for instantiating weather type cards in Gwent.
+ *
+ *  @param name The name of the card.
+ *  */
+class WeatherCard(name: String) extends Card(name) {
+}
