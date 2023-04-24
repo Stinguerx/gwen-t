0d883e210cf4dd49cc98d9ff728bbf2e9d9d0628
Stinguerx <stinguerx.soc75@gmail.com>
2023-04-24 14:59:11 -0400

    Added missing methods, parameter enforcement and fixed reference issues

--- src/main/scala/gwent/Player.scala
+++ src/main/scala/gwent/Player.scala
@@ -1,6 +1,8 @@
 package cl.uchile.dcc
 package gwent
 
+import cards.Card
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 /** Class that defines all attributes and behaviours of a player and is used to instantiate a player in Gwent.
@@ -9,11 +11,12 @@ import scala.util.Random
  *  @param name The name of the player (type: String).
  *  @param deck The initial deck of cards for the player, a deck of 25 cards must be given (type: List[Card]).
  */
-class Player(private val _name: String, private var _deck: List[Card]) {
+class Player(private val _name: String, private val initialDeck: ArrayBuffer[Card]) extends Equals {
   /** Player initialization */
-  require(_deck.size == 25, "The deck given must have 25 cards")
+  require(initialDeck.size == 25, "The deck given must have 25 cards")
+  private var _deck: ArrayBuffer[Card] = initialDeck.clone()
   private var _gems: Int = 2
-  private var _hand: List[Card] = List()
+  private var _hand: ArrayBuffer[Card] = ArrayBuffer.empty[Card]
   shuffleDeck()
   drawCards(10)
   
@@ -31,14 +34,14 @@ class Player(private val _name: String, private var _deck: List[Card]) {
   }
 
   /** @return The list of cards in the player's hand. */
-  def hand: List[Card] = _hand
+  def hand: ArrayBuffer[Card] = _hand
 
   /** @return the list of cards in the player's deck. */
-  def deck: List[Card] = _deck
+  def deck: ArrayBuffer[Card] = _deck
 
   /** Shuffles the player's deck. */
   def shuffleDeck(): Unit = {
-    _deck = Random.shuffle(_deck)
+    Random.shuffle(_deck)
   }
 
   /** Draws the specified number of cards from the player's deck and puts them in the player's hand.
@@ -48,14 +51,40 @@ class Player(private val _name: String, private var _deck: List[Card]) {
    *  @param cards The number of cards to be drawn (type: Int, default = 1).
    * */
   def drawCards(cards: Int = 1): Unit = {
-    for (_ <- 1 to cards) {
+    val numCards = math.min(cards, 25 - _hand.size)
+    for (_ <- 0 until numCards) {
       if (_deck.nonEmpty) {
         val drawnCard: Card = _deck.last
-        _deck = _deck.dropRight(1)
-        _hand = _hand :+ drawnCard
+        _deck.dropRightInPlace(1)
+        _hand += drawnCard
       }
-      else break
     }
+  }
+
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Player]
+
+  override def equals(that: Any): Boolean = {
+    if (canEqual(that)) {
+      val other = that.asInstanceOf[Player]
+      (this eq other) || (
+        this.name == other.name &&
+          this.gems == other.gems &&
+          this._deck == other._deck &&
+          this.hand.toSet == other.hand.toSet
+        )
+    } else false
+
+  }
+
+  override def hashCode(): Int = {
+    val prime = 31
+    var result = 1
+    result = prime * result + classOf[Player].##
+    result = prime * result + name.##
+    result = prime * result + _gems.##
+    result = prime * result + _deck.##
+    result = prime * result + _hand.toSet.##
+    result
   }
 }
 
