deea1cfbcfe661c4d29898ba24ee021ea9812176
Stinguerx <Stinguerx@users.noreply.github.com>
2023-04-24 23:28:44 -0400

    Created IPlayer trait

---
+++ src/main/scala/gwent/IPlayer.scala
@@ -0,0 +1,17 @@
+package cl.uchile.dcc
+package gwent
+
+trait IPlayer {
+
+  /** Draws the specified number of cards from the player's deck and puts them in the player's hand.
+   * If the number specified is greater than the number of cards available in the player's deck, it draws the maximum
+   * number of cards it can before the deck becomes empty.
+   *
+   * @param cards The number of cards to be drawn (type: Int, default = 1).
+   * */
+  def drawCards(cards: Int) : Unit
+
+  // Plays a card from the player's hand into the game board, according to the card type.
+  // @param position The position of the card to be played in the player's hand.
+  // def playCard(position: Int) : Unit
+}
