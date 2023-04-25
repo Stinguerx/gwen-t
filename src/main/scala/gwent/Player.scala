3e4fddab608890102936a801299eef53cd5e51ca
Stinguerx <Stinguerx@users.noreply.github.com>
2023-04-24 23:30:32 -0400

    Made Player class extend IPlayer trait

--- src/main/scala/gwent/Player.scala
+++ src/main/scala/gwent/Player.scala
@@ -5,13 +5,14 @@ import cards.Card
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
-/** Class that defines all attributes and behaviours of a player and is used to instantiate a player in Gwent.
- *  The player starts with 2 gems, its initial deck is shuffled, and then 10 cards are drawn from it to the player's hand
+/** Class that implements the IPlayer trait and defines all attributes and behaviours of a player and is used to
+ *  instantiate a player in Gwent. The player starts with 2 gems, its initial deck is shuffled, and then 10 cards
+ *  are drawn from it to the player's hand.
  *
  *  @param name The name of the player (type: String).
  *  @param deck The initial deck of cards for the player, a deck of 25 cards must be given (type: List[Card]).
  */
-class Player(private val _name: String, private val initialDeck: ArrayBuffer[Card]) extends Equals {
+class Player(private val _name: String, private val initialDeck: ArrayBuffer[Card]) extends IPlayer with Equals {
   /** Player initialization */
   require(initialDeck.size == 25, "The deck given must have 25 cards")
   private var _deck: ArrayBuffer[Card] = initialDeck.clone()
@@ -44,12 +45,6 @@ class Player(private val _name: String, private val initialDeck: ArrayBuffer[Car
     Random.shuffle(_deck)
   }
 
-  /** Draws the specified number of cards from the player's deck and puts them in the player's hand.
-   *  If the number specified is greater than the number of cards available in the player's deck, it draws the maximum
-   *  number of cards it can before the deck becomes empty.
-   *
-   *  @param cards The number of cards to be drawn (type: Int, default = 1).
-   * */
   def drawCards(cards: Int = 1): Unit = {
     val numCards = math.min(cards, 25 - _hand.size)
     for (_ <- 0 until numCards) {
