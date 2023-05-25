c6bf91b907cf5ebe88ac612dbc32f53a47fc5660
Stinguerx <Stinguerx@users.noreply.github.com>
2023-05-24 23:50:25 -0400

    Added playCard and shuffleDeck methods to IPlayer trait

--- src/main/scala/gwent/IPlayer.scala
+++ src/main/scala/gwent/IPlayer.scala
@@ -1,9 +1,15 @@
 package cl.uchile.dcc
 package gwent
 
+import gwent.cards.Card
+
+import scala.collection.mutable.ArrayBuffer
+
+/** Trait that defines all the common behaviours of a player in the game Gwent */
 trait IPlayer {
 
-  /** Draws the specified number of cards from the player's deck and puts them in the player's hand.
+  /**
+   * Draws the specified number of cards from the player's deck and puts them in the player's hand.
    * If the number specified is greater than the number of cards available in the player's deck, it draws the maximum
    * number of cards it can before the deck becomes empty.
    *
@@ -11,7 +17,15 @@ trait IPlayer {
    * */
   def drawCards(cards: Int) : Unit
 
-  // Plays a card from the player's hand into the game board, according to the card type.
-  // @param position The position of the card to be played in the player's hand.
-  // def playCard(position: Int) : Unit
+  /**
+   * Plays a card from the player's hand into the game board, according to the card type.
+   * Throws an error if the player doesn't have a board assigned to it, or the card in the position specified does not
+   * exist.
+   *
+   * @param position The position of the card to be played in the player's hand.
+   * */
+  def playCard(position: Int) : Unit
+
+  /** Shuffles the player's deck. */
+  def shuffleDeck(): Unit
 }
