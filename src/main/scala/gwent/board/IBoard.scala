0da4e3bb16f78d0a3d86755bb81da0b6949cba41
Stinguerx <Stinguerx@users.noreply.github.com>
2023-05-24 23:49:47 -0400

    Created IBoard trait

---
+++ src/main/scala/gwent/board/IBoard.scala
@@ -0,0 +1,21 @@
+package cl.uchile.dcc
+package gwent.board
+
+import gwent.cards._
+import gwent.Player
+
+/**
+ * Trait that contains the common methods of a board in the game Gwent.
+ * */
+trait IBoard {
+
+  /**
+   * Receives the card the player wants to play and puts it in the correct zone according to the section of the
+   * board assigned to the player.
+   *
+   * @param player the player that wants to place a card.
+   * @param card the card the player wants to place in the board.
+   * */
+  def placeCard(player: Player, card: Card): Unit
+
+}
