ef8d44d147d2dc503216bf6f8d496cf7cd397893
Stinguerx <Stinguerx@users.noreply.github.com>
2023-05-29 17:04:11 -0400

    Moved default methods to ICard trait

--- src/main/scala/gwent/cards/Card.scala
+++ src/main/scala/gwent/cards/Card.scala
@@ -1,22 +1,9 @@
 package cl.uchile.dcc
 package gwent.cards
 
-import gwent.board.CardVisitor
 
-import gwent.Player
-
-/** Abstract class that defines the common attributes of all cards in Gwent. */
-abstract class Card(protected val _name: String, protected val _description: String) extends Equals{
-  /** @return The name of the card.*/
-  def name: String = _name
-
-  /** @return The description of the card */
-  def description: String = _description
-
-  /** Calls the appropriate method on the CardPlacer class according to the type of the card, in order to handle
-   *  the placement of the card on the board.
-   * */
-  def accept(visitor: CardVisitor, player: Player): Unit
+/** Abstract class that implements the ICard trait and defines the common attributes of all cards in Gwent. */
+abstract class Card(protected val _name: String, protected val _description: String) extends ICard with Equals{
 
 }
 
