f08f56b9875eef02c1ea133499d2a9e5e99d7493
Stinguerx <Stinguerx@users.noreply.github.com>
2023-06-07 22:47:20 -0400

    Added basics tests for game controller

---
+++ src/test/scala/gwent/GameControllerTest.scala
@@ -0,0 +1,67 @@
+package cl.uchile.dcc
+package gwent
+
+import gwent.cards._
+import gwent.controller._
+import gwent.view._
+
+import gwent.controller.states._
+import munit.FunSuite
+
+import scala.collection.mutable.ArrayBuffer
+class GameControllerTest extends FunSuite {
+
+  var testDeck: ArrayBuffer[ICard] = ArrayBuffer(
+    new MeleeCard("Card 1", "desc", 1),
+    new MeleeCard("Card 2", "desc", 2),
+    new RangedCard("Card 3", "desc", 3),
+    new RangedCard("Card 4", "desc", 4),
+    new SiegeCard("Card 5", "desc", 5),
+    new SiegeCard("Card 6", "desc", 6),
+    new WeatherCard("Card 7", "desc"),
+    new MeleeCard("Card 8", "desc", 1),
+    new MeleeCard("Card 9", "desc", 2),
+    new RangedCard("Card 10", "desc", 3),
+    new RangedCard("Card 11", "desc", 4),
+    new SiegeCard("Card 12", "desc", 5),
+    new SiegeCard("Card 13", "desc", 6),
+    new WeatherCard("Card 14", "desc"),
+    new MeleeCard("Card 15", "desc", 1),
+    new MeleeCard("Card 16", "desc", 2),
+    new RangedCard("Card 17", "desc", 3),
+    new RangedCard("Card 18", "desc", 4),
+    new SiegeCard("Card 19", "desc", 5),
+    new SiegeCard("Card 20", "desc", 6),
+    new WeatherCard("Card 21", "desc"),
+    new MeleeCard("Card 22", "desc", 1),
+    new MeleeCard("Card 23", "desc", 2),
+    new RangedCard("Card 24", "desc", 3),
+    new RangedCard("Card 25", "desc", 4)
+  )
+
+  var player1: Player = _
+  var player2: Player = _
+  var game: GameController = _
+  val userInterface = new ConsoleInterface()
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    player1 = new Player("player1", testDeck)
+    player2 = new Player("player2", testDeck)
+    game = new GameController(userInterface)
+  }
+
+  test("Game Controller is well initialized") {
+    assertEquals(game.userInterface, userInterface)
+  }
+
+  test("addPlayers works correctly") {
+    game.addPlayers(player1, player2)
+    assertEquals(game.getPlayers, List(player1, player2))
+  }
+
+  test("startGame works correctly") {
+    game.startGame()
+    assert(game.state.isInstanceOf[Player1TurnState])
+  }
+
+}
