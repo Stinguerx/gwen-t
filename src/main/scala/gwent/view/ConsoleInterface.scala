ac04ef2a61245541dfd32281d53de3a08b2f939e
Stinguerx <Stinguerx@users.noreply.github.com>
2023-07-17 18:14:42 -0400

    Created user interface

--- src/main/scala/gwent/view/ConsoleInterface.scala
+++ src/main/scala/gwent/view/ConsoleInterface.scala
@@ -1,6 +1,11 @@
 package cl.uchile.dcc
 package gwent.view
 
+import scala.io.StdIn
+import scala.util.Random
+
 class ConsoleInterface extends UserInterface {
-  override def promptPlayer(): Int = ???
+  override def promptPlayer(maximum: Int): Int = {
+    Random.nextInt(maximum + 1)
+  }
 }
