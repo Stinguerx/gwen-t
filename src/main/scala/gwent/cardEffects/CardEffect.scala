5c65a37822a923a486026ee556b3bafd9357bfdf
Stinguerx <Stinguerx@users.noreply.github.com>
2023-07-17 18:17:52 -0400

    Added docs and small refactor

--- src/main/scala/gwent/cardEffects/CardEffect.scala
+++ src/main/scala/gwent/cardEffects/CardEffect.scala
@@ -1,19 +1,26 @@
 package cl.uchile.dcc
 package gwent.cardEffects
 
-import gwent.cards.{ICard, UnitCard}
+import gwent.cards.{ICard, IUnitCard}
 
+import scala.collection.mutable.ArrayBuffer
 
+/** Class that implements the ICardEffect trait
+ *  @param name The name of the effect
+ *  @param description The description of the effect
+ *  @param effect The effect function that will applied to the cards
+ *  @param affectSelf Whether or not the card that holds the effect should be affected
+ * */
 class CardEffect(protected val _name: String,
                  protected val _description: String,
-                 val effect: (ICard, UnitCard) => Unit,
-                 val selfEffect: Boolean) extends ICardEffect {
+                 val effect: (ICard, ArrayBuffer[_ <: IUnitCard]) => Unit,
+                 ) extends ICardEffect {
 
   def name: String = _name
 
   def description: String = _description
 
-  def applyEffect(card: ICard, affectedCard: UnitCard): Unit = {
-    effect(card, affectedCard)
+  def applyEffect(originCard: ICard, targetCards: ArrayBuffer[_ <: IUnitCard]): Unit = {
+    effect(originCard, targetCards)
   }
 }
--- src/main/scala/gwent/cardEffects/ICardEffect.scala
+++ src/main/scala/gwent/cardEffects/ICardEffect.scala
@@ -3,15 +3,17 @@ package gwent.cardEffects
 
 import gwent.cards._
 
+import scala.collection.mutable.ArrayBuffer
 
+/** Trait exposing the common attributes and behaviour of an card effect in Gwent. */
 trait ICardEffect {
 
   protected val _name: String
 
   protected val _description: String
 
-  val effect: (ICard, UnitCard) => Unit
+  val effect: (ICard, ArrayBuffer[_ <: IUnitCard]) => Unit
 
-  def applyEffect(card: ICard, affectedCard: UnitCard): Unit
+  def applyEffect(originCard: ICard, targetCards: ArrayBuffer[_ <: IUnitCard]): Unit
 
 }
