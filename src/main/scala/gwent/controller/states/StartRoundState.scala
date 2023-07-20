c811f4f322e1e0735cf0a9232f8c707c92ee9770
Stinguerx <Stinguerx@users.noreply.github.com>
2023-07-19 22:12:46 -0400

    Added missing logic

--- src/main/scala/gwent/controller/states/StartRoundState.scala
+++ src/main/scala/gwent/controller/states/StartRoundState.scala
@@ -8,6 +8,8 @@ class StartRoundState(context: GameController) extends GameState(context) {
 
   override def startRound(): Unit = {
     context.board.clearBoard()
+    context.Player1.shuffleDeck()
+    context.Player2.shuffleDeck()
     context.Player1.drawCards(3)
     context.Player2.drawCards(3)
     context.state = new Player1TurnState(context)
